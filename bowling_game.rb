c40b4594dc9076a66dc2e2b463581dc4974f3953
Jesse Kevon <jessekevon@Jesses-MacBook-Pro.local>
2013-09-12 11:24:45 -0500

    adds strike and spare methods and tests

--- bowling_game.rb
+++ bowling_game.rb
@@ -18,6 +18,30 @@ class BowlingGame
 # Returns the Integer score for this game.
 
   def score
-    @rolls.reduce(:+)
+    #@rolls.reduce(:+)
+
+    total_score = 0
+    current_roll = 0
+
+    while current_roll < @rolls.size - 1
+      roll = @rolls[current_roll]
+      next_roll = @rolls[current_roll + 1]
+
+      if roll == 10                                    #if strike
+        total_score += 10 + @rolls[current_roll + 1] + @rolls[current_roll + 2]
+        current_roll += 1
+
+      elsif roll + next_roll == 10                    #if frame is a spare
+
+        total_score += 10 + @rolls[current_roll + 2]
+        current_roll += 2
+      else                                             #if frame is neither strike nor spare
+
+        total_score += roll + next_roll
+        current_roll += 2
+      end
+    end
+
+    return total_score
   end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -4,26 +4,47 @@ require 'rspec'
 require_relative '../bowling_game'
 
 describe BowlingGame do
+  let (:game) {BowlingGame.new}
   describe '.initialize' do 
     it 'sets @rolls to an empty array' do
-      game = BowlingGame.new
 
       expect(game.rolls).to eq([])
     end
   end
 
   describe '#score' do
-    it 'scores the game as 0 when only gutter balls are thrown' do
-      game = BowlingGame.new
-      20.times { game.roll(0) }
+    context 'When only gutter balls are thrown' do
+      it 'scores the game as 0' do
+        20.times { game.roll(0) }
 
-      expect(game.score).to eq(0)
+        expect(game.score).to eq(0)
+      end
     end
-    it 'scores the game as 20 when one pin in knocked down on each roll' do
-      game = BowlingGame.new
-      20.times { game.roll(1) }
+    context 'when 1 pin is knocked down each roll' do
+      it 'scores the game as 20' do
+        20.times { game.roll(1) }
 
-      expect(game.score).to eq(20)
+        expect(game.score).to eq(20)
+      end
+    end
+    context 'When a spare is thrown' do
+      it 'records the frame score as 10 plus the number of pins knocked down in the next roll' do
+        3.times {game.roll(5)}
+        17.times {game.roll(0)}
+
+        expect(game.score).to eq(20)
+      end
+    end
+
+    context'When a strike is thrown' do
+      it 'records the frame score as 10 plus the total number of pins knocked down in the next two rolls' do
+        game.roll(10)
+        game.roll(7)
+        game.roll(1)
+        16.times { game.roll(0) }
+
+        expect(game.score).to eq(26)
+      end
     end
   end
 end
